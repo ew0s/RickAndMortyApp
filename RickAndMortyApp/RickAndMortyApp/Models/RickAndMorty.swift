8b202803ed082d049d99cf8fcdbbdc421a300a9d
ew0s <ewos365@mail.ru>
2021-05-05 16:50:50 +0300

    Add: rickAndMorty model

---
+++ RickAndMortyApp/RickAndMortyApp/Models/RickAndMorty.swift
@@ -0,0 +1,48 @@
+//
+//  Character.swift
+//  RickAndMortyApp
+//
+//  Created by Егор Савковский on 05.05.2021.
+//
+
+struct RickAndMorty: Decodable {
+    let info: Info
+    let results: [Character]
+}
+
+struct Info: Decodable {
+    let pages: Int
+    let next: String?
+    let prev: String?
+}
+
+struct Character: Decodable {
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: Location
+    let location: Location
+    let image: String?
+    let episode: [String]
+    let url: String
+    
+    var description: String {
+        """
+        Name: \(name)
+        Status: \(status)
+        Species: \(species)
+        Gender: \(gender)
+        Origin: \(origin)
+        Location: \(location.name)
+        """
+    }
+}
+
+struct Location: Decodable {
+    let name: String
+}
+
+enum URLS: String {
+    case rickAndMortyApi = "https://rickandmortyapi.com/api/character"
+}
