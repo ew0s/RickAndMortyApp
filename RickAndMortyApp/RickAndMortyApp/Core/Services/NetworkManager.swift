b7bf12f46d8857fa7e5628c514a601e340e1914f
ew0s <ewos365@mail.ru>
2021-05-09 02:31:37 +0300

    Fix: NetworkManager logic using almofire

--- RickAndMortyApp/RickAndMortyApp/Core/Services/NetworkManager.swift
+++ RickAndMortyApp/RickAndMortyApp/Core/Services/NetworkManager.swift
@@ -5,7 +5,7 @@
 //  Created by Егор Савковский on 05.05.2021.
 //
 
-import Foundation
+import Alamofire
 
 class NetworkManager {
     
@@ -14,90 +14,50 @@ class NetworkManager {
     
     // MARK: - Public methods
     func fetchData(from url: String?, with complition: @escaping (RickAndMorty) -> Void) {
-        guard let stringURL = url else { return }
-        guard let url = URL(string: stringURL) else { return }
         
-        URLSession.shared.dataTask(with: url) { data,_,error in
-            if let error = error {
-                print(error)
-                return
-            }
-            
-            guard let data = data else { return }
-            
-            do {
-                let rickAndMorty = try JSONDecoder().decode(RickAndMorty.self, from: data)
-                DispatchQueue.main.async {
-                    complition(rickAndMorty)
+        guard let stringURL = url else { return }
+        AF.request(stringURL)
+            .validate()
+            .responseDecodable(of: RickAndMorty.self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let data):
+                    complition(data)
+                case .failure(let error):
+                    print(error)
                 }
-            } catch let error {
-                print(error)
             }
-            
-        }.resume()
     }
     
     func fetchEpisode(from url: String?, completion: @escaping(Episode) -> Void) {
-        guard let stringUrl = url else { return }
-        guard let url = URL(string: stringUrl) else { return }
         
-        URLSession.shared.dataTask(with: url) { (data, _, error) in
-            guard let data = data else {
-                print(error ?? "no descripption")
-                return
-            }
-            
-            do {
-                let episode = try JSONDecoder().decode(Episode.self, from: data)
-                DispatchQueue.main.async {
-                    completion(episode)
+        guard let stringURL = url else { return }
+        AF.request(stringURL)
+            .validate()
+            .responseDecodable(of: Episode.self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let data):
+                    completion(data)
+                case .failure(let error):
+                    print(error)
                 }
-            } catch let error {
-                print(error)
             }
-        }.resume()
     }
     
     func fetchCharacter(from url: String?, with complition: @escaping (Character) -> Void) {
-        guard let stringURL = url else { return }
-        guard let url = URL(string: stringURL) else { return }
         
-        URLSession.shared.dataTask(with: url) { data,_,error in
-            if let error = error {
-                print(error)
-                return
-            }
-            
-            guard let data = data else { return }
-            
-            do {
-                let character = try JSONDecoder().decode(Character.self, from: data)
-                DispatchQueue.main.async {
-                    complition(character)
+        guard let stringURL = url else { return }
+        AF.request(stringURL)
+            .validate()
+            .responseDecodable(of: Character.self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let data):
+                    complition(data)
+                case .failure(let error):
+                    print(error)
                 }
-            } catch let error {
-                print(error)
             }
-            
-        }.resume()
     }
     
     // MARK: - Initialization
     private  init() {}
-}
-
-class NetworkImageManager {
-    
-    // MARK: - Public properties
-    static let shared = NetworkImageManager()
-    
-    // MARK: - Public methods
-    func fetchImage(from url: String?) -> Data? {
-        guard let stringURL = url else { return nil }
-        guard let url = URL(string: stringURL) else { return nil }
-        return try? Data(contentsOf: url)
-    }
-    
-    // MARK: - Initialization
-    private init() {}
 }
